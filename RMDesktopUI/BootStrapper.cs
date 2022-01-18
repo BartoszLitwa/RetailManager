7cc0c55a8190cdb811214c884e27311a829c44a0
CRNYY <45899905+CRNYY@users.noreply.github.com>
2022-01-18 13:12:59 +0100

    Adds the SQL Database Project and setup scheme
    Adds the WPF Desktop UI with Caliburn.Micro and some initial setup

---
+++ RMDesktopUI/BootStrapper.cs
@@ -0,0 +1,25 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI
+{
+    class BootStrapper : BootstrapperBase
+    {
+        public BootStrapper()
+        {
+            Initialize();
+        }
+        
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Replace default MainWindow view with ShellWindow
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
