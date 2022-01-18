54fceee6c68f3ee137f460e650e1459f5c0ca99c
CRNYY <45899905+CRNYY@users.noreply.github.com>
2022-01-18 17:11:34 +0100

    Register DB Tables
    The tables for the basic register system

---
+++ RMData/dbo/Tables/Inventory.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Inventory]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductId] INT NOT NULL, 
+    [Quantity] INT NOT NULL, 
+    [PurchasePrice] MONEY NOT NULL, 
+    [PurchaseDate] DATETIME2 NOT NULL,
+
+)
---
+++ RMData/dbo/Tables/ProductTable.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ProductTable]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductName] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+    [RetailPrice] MONEY NOT NULL,
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate(),
+
+)
